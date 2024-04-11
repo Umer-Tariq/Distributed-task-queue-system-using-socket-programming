de258971f04e44eed16886edb1dc13350a98eb54
Umer-Tariq <K213261@nu.edu.pk>
2024-04-11 16:05:32 +0500

    Sender and Controller changed

--- controller.py
+++ controller.py
@@ -1,32 +1,53 @@
 import socket
 import os
+import time
 
-hashmap = { "receiver_1" : 9996 }
+#hashmap = { "receiver_1" : 9996 }
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("localhost", 9999))
+server.bind(("localhost", 9998))
 server.listen()
 client, addr = server.accept()
-rec = client.recv(10).decode()
-port = hashmap[rec]
-file_name = client.recv(20).decode()
+#rec = client.recv(1.decode()
+#port = hashmap[rec]
+#file_name = client.recv(20).decode()
 #file_size = client.recv(4).decode()
 
-done = "False"
+#client2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#client2.connect(("localhost", port))
 
-client2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client2.connect(("localhost", port))
-
-client2.send(file_name.encode())
-print(file_name)
+#client2.send(file_name.encode())
+#print(file_name)
 #client2.send(str(file_size).encode())
+reviews = []
+current_review = ''
 
-while done == 'False':
-    data = client.recv(1024)
+while True:
+    try:
+        data = client.recv(1024).decode()
+    except UnicodeDecodeError as e:
+        print("UnicodeDecodeError:", e)
+        print("Skipping this data.")
+        continue  # Skip processing this data
 
-    if data[-5:] == b"<END>":
-        done = True
+    if not data:  # Check if no data is received
+        break  # Exit the loop
+    if '<END>' in data:
+        parts = data.split('<END>')  # Split data using '<END>' as the delimiter
+        current_review += parts[0]  # Append the first part to the current_review
+        reviews.append(current_review)  # Add the complete review to reviews
+        current_review = parts[1]  # Start a new review with the second part
+    else:
+        current_review += data  # Append data to the current_review
 
-    client2.sendall(data)
 
+#print("Received reviews:")
+for i in range(0, len(reviews)):
+    print(i)
+    print(reviews[i])
+    time.sleep(1)
+
+#print(reviews[1])
+
+client.close()
 server.close()
-client2.close()
+#client2.close()
--- sender1.py
+++ sender1.py
@@ -1,20 +1,31 @@
 import os
 import socket
+import time
+import pandas as pd
+df = pd.read_excel('movies.xlsx')
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("localhost", 9999))
+client.connect(("localhost", 9998))
 
-file = open("sample2.txt", "rb")
-file_size = os.path.getsize("sample2.txt")
-print(file_size)
-client.send("receiver_1".encode())
-client.send("received_sample2.txt".encode())
-#client.send(str(file_size).encode())
-while True:
-    data = file.read(1024)
-    if not data:
-        break
+#file_size = os.path.getsize("sample2.txt")
+#print(file_size)
+#client.send("receiver_1".encode())
+#client.send("received_sample2.txt".encode())
+##client.send(str(file_size).encode())
+#while True:
+#    data = file.read(1024)
+#    if not data:
+#        break
 
-    client.sendall(data)
-client.send(b"<END>")
-file.close()
+#    client.sendall(data)
+#client.send(b"<END>")
+#file.close()
+im = 0
+for i in df['review']:
+    print(im)
+    print(i)
+    print('--------')
+    client.sendall(i.encode())
+
+    client.sendall("<END>".encode())
+    im += 1
 client.close()
