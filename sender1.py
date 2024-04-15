9390f9a85abd91b85e247efd220e0bbb53dea45d
saroshirfan2002 <166515100+saroshirfan2002@users.noreply.github.com>
2024-04-15 18:48:17 +0500

    sender1.py milestone 2

--- sender1.py
+++ sender1.py
@@ -1,33 +1,44 @@
-import os
 import socket
-import time
 import pandas as pd
+import threading
 
+num_senders = 5
+m = []
 df = pd.read_excel('movies.xlsx')
+for i in df['review']:
+    i = i + '#END@'
+    m.append(i)
+
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("localhost", 9999))
 
-#file_size = os.path.getsize("sample2.txt")
-#print(file_size)
-#client.send("receiver_1".encode())
-#client.send("received_sample2.txt".encode())
-##client.send(str(file_size).encode())
-#while True:
-#    data = file.read(1024)
-#    if not data:
-#        break
+def sender_func(lowerbound, upperbound):
+    for i in range(lowerbound, upperbound + 1):
+        lock.acquire()
+        client.sendall(m[i].encode())
+        lock.release()
 
-#    client.sendall(data)
-#client.send(b"<END>")
-#file.close()
-im = 0
-for i in df['review']:
-    #print(im)
-    #print(i)
-    print('--------')
-    client.sendall(i.encode())
+total_rows = len(m) - 1  # Subtract 1 for the heading row
+rows_per_sender = total_rows // num_senders  # Calculate rows per sender
 
-    client.sendall("<END>".encode())
-    im += 1
-print(im - 1)
+sender_threads = []
+lock = threading.Lock()
+lb = 0  # Start from row 1 (excluding heading row)
+ub = lb + rows_per_sender
+print('len of df is ', len(df))
+for i in range(0, num_senders):
+    sender_thread = threading.Thread(target=sender_func, args=(lb, ub))
+    sender_threads.append(sender_thread)
+    lb = ub + 1  # Move to the next set of rows for the next sender
+    ub = lb + rows_per_sender 
+    if ub >= len(df):
+        ub = len(df) - 1  # Handle the last sender
+
+for thread in sender_threads:
+    thread.start()
+
+for thread in sender_threads:
+    thread.join()
+
 client.close()
