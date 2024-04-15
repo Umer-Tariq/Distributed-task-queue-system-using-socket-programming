83de859150a29fb5736d550737754b5ece546af9
saroshirfan2002 <166515100+saroshirfan2002@users.noreply.github.com>
2024-04-15 18:50:01 +0500

    reciever1.py 2nd milestone

--- reciever1.py
+++ reciever1.py
@@ -1,8 +1,7 @@
 import socket
 import os
 import nltk
-
-
+import threading
 
 #function to classify a review as positiv or negative. Expects one singal review as input. Returns it's class.
 def classify_review(paragraph):
@@ -21,6 +20,13 @@ def classify_review(paragraph):
 
     return sentiment
 #establishing connection via port 9996. the connection is established between the controller and the reciever
+
+def calc_Sentiment(result, lb, ub, Sentiments):
+    for i in range(lb, ub + 1):
+        Sentiments[i] = classify_review(result[i])
+
+
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(("localhost", 9996))
 server.listen()
@@ -56,3 +62,26 @@ if current_result:  # Check if there's any remaining data in current_result
 print(len(result))
 client.close()
 server.close()
+
+Sentiments = ['0'] * len(result)
+sender_threads = []
+#calculaTing senTimenT analsis on resulTs
+num_threads = 5
+total_rows = len(result)
+rows_per_sender = total_rows // num_threads 
+#print(rows_per_sender)
+lb = 0
+ub = lb + rows_per_sender
+for i in range(0, num_threads):
+    sender_thread = threading.Thread(target=calc_Sentiment, args=(result, lb, ub, Sentiments))
+    sender_threads.append(sender_thread)
+    lb = ub + 1  # Move to the next set of rows for the next sender
+    ub = lb + rows_per_sender 
+    if ub >= len(result):
+        ub = len(result) - 1
+
+for thread in sender_threads:
+    thread.start()
+
+for thread in sender_threads:
+    thread.join()
